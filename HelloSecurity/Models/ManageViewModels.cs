660504b332d813e050ff7e79ac0073aeb6daaa5f
docampo <docampo@DOCAMPO-PC>
2020-01-16 14:34:44 -0800

    test

--- HelloSecurity/Models/ManageViewModels.cs
+++ HelloSecurity/Models/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace HelloSecurity.Models
+namespace HelloSecurity.Models//test
 {
     public class IndexViewModel
     {
